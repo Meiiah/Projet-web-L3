8837e0b9f71ba97e8d5cc1a8fc23419e4a1834c1
Philippe Marion <s163750@ic2s120-04.univ-lemans.fr>
2019-04-08 11:55:38 +0200

    ajout du tri dans le json

--- public/js/monprogrammeamwa.js
+++ public/js/monprogrammeamwa.js
@@ -7,6 +7,8 @@ var bot = new Bot(config1);
 var jsonfile = require('jsonfile')
 var notreId = '1105484789044465665'
 var index=0;
+
+var data = [];
 //get date string for today's date (e.g. '2011-01-01')
 function datestring () {
   var d = new Date(Date.now() - 5*60*60*1000);  //est timezone
@@ -22,21 +24,58 @@ function datestring () {
     , result_type: 'mixed'
   };
 
-  bot.twit.get('friends/ids', params, function (err, reply) {
+  bot.twit.get('followers/ids', params, function (err, reply) {
+  //  console.log('\n# followers:' + reply.ids.length.toString()  );
+  });
+
+  bot.twit.get('search/tweets', params, function (err, reply) {
 
     if(err) return handleError(err)
-    //console.log('\n# followers:' + reply.ids.length.toString());
+    //
+     for (var i = 0; i < reply.statuses.length; i++) {
+       console.log("Localisation:")
+       console.log(reply.statuses[i].user.location);
+       data.push({'localisation': reply.statuses[i].user.location});
+       console.log("Text du tweet:")
+       console.log(reply.statuses[i].text);
+        data.push({'contenu': reply.statuses[i].text});
+       console.log("Hashtag:")
+       console.log(reply.statuses[i].entities.hashtags);
+       data.push({'hashtag': reply.statuses[i].entities.hashtags});
+       console.log("User retweet:")
+       console.log(reply.statuses[i].entities.user_mentions);
+       console.log("fin user")
+       console.log("Id du createur du tweet:")
+       console.log(reply.statuses[i].user.id_str);
+       console.log("Color:")
+       console.log(reply.statuses[i].user.profile_sidebar_border_color);
+       console.log(reply.statuses[i].user.profile_background_color);
+       console.log(reply.statuses[i].user.profile_sidebar_fill_color);
+       console.log("nb Jaime:")
+       console.log(reply.statuses[i].favorite_count);
+       console.log("nb retweet:");
+       console.log(reply.statuses[i].retweet_count);
+       console.log("nb de tweeet que tu as fais:")
 
-   //  for (var i = 0; i < reply.statuses.length; i++) {
-  //  console.log(reply.statuses[i].id);
-    //}
+       console.log(reply.statuses[i].user.statuses_count);
+       console.log("date");
+       console.log(reply.statuses[i].created_at);
+      console.log(reply.statuses[i].quote_count);
+
+    //  for (var name in goals) {
+    //  if (goals.hasOwnProperty(name)) {
+
+      //  }
+    //  }
+
+}
 
 
-    console.log(reply);
+   //console.log(reply);
 
-  //  if reply_id == nous {
-      //  jsonfile.writeFile('../json/data.json',JSON.stringify(reply),'utf8', err)
-  //  }
+
+   jsonfile.writeFile('../json/data.json',JSON.stringify(data),'utf8', err);
+
 
   });
   //var rand = Math.random();
